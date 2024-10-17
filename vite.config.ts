0d537a1c4d5839451b2fc2e1789e62ea6fcae1c9
Kazumi Inada <hello@nandenjin.com>
2024-10-17 17:02:01 +0900

    feat: PWAのサポート (#2)

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,55 @@
-import type { UserConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
-export default {
-  root: 'src',
-  plugins: [vue()],
-  build: {
-    outDir: '../dist',
-  },
-} satisfies UserConfig
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+  return {
+    root: 'src',
+    base: env.VITE_BASE,
+    plugins: [
+      vue(),
+      VitePWA({
+        registerType: 'autoUpdate',
+        manifest: {
+          name: '@tsukuba-neu/sawagani',
+          short_name: 'sawagani',
+          theme_color: '#e34944',
+          scope: env.VITE_BASE,
+          start_url: env.VITE_BASE,
+          icons: [
+            {
+              src: 'pwa-64x64.png',
+              sizes: '64x64',
+              type: 'image/png',
+            },
+            {
+              src: 'pwa-192x192.png',
+              sizes: '192x192',
+              type: 'image/png',
+            },
+            {
+              src: 'pwa-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+              purpose: 'any',
+            },
+            {
+              src: 'maskable-icon-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+              purpose: 'maskable',
+            },
+          ],
+        },
+        pwaAssets: {
+          image: 'public/favicon.svg',
+        },
+      }),
+    ],
+    build: {
+      outDir: '../dist',
+      emptyOutDir: true,
+    },
+  }
+})
