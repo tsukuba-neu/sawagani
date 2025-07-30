060886641553959cffd56cf9e0927711782091ee
Kazumi Inada <hello@nandenjin.com>
2025-07-30 18:38:00 +0900

    fix: undefinedな値を全角半角変換しないようにチェック

--- src/store/data.ts
+++ src/store/data.ts
@@ -118,12 +118,16 @@ export const useDataStore = defineStore('data', () => {
             row[header.indexOf('支出')],
           ),
         ),
-        receipt: replaceFullWidthWithHalfWidth(row[header.indexOf('領収書No')]),
+        receipt: replaceFullWidthWithHalfWidth(
+          row[header.indexOf('領収書No')] || '',
+        ),
         recipient: row[header.indexOf('謝礼相手先')],
         transportPurpose: row[header.indexOf('通信運搬用途')],
         printPurpose: row[header.indexOf('印刷目的')],
         owner: row[header.indexOf('用具所有者')],
-        numStay: replaceFullWidthWithHalfWidth(row[header.indexOf('延べ宿泊数')]),
+        numStay: replaceFullWidthWithHalfWidth(
+          row[header.indexOf('延べ宿泊数')] || '',
+        ),
       }
 
       result.push(transaction)
